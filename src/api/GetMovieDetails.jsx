51961405ea56db558e0548672cac545f964cbb0a
AnirbanSaha18 <anisaha2001@gmail.com>
2025-04-25 16:25:13 +0530

    Fifth Commit

--- src/api/GetMovieDetails.jsx
+++ src/api/GetMovieDetails.jsx
@@ -3,7 +3,7 @@ export const getMoviesDetails = async({params})=>{
     const id = params.movieID;
     try{
         const response = await fetch(
-           `http://www.omdbapi.com/?i=${id}&apikey=${import.meta.env.VITE_API_KEY}`
+           `https://www.omdbapi.com/?i=${id}&apikey=${import.meta.env.VITE_API_KEY}`
 
         );
         console.log("Movie loader running");
