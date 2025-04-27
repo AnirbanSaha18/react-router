77abc80715f4fda3df316bd63e161969ee76beba
AnirbanSaha18 <anisaha2001@gmail.com>
2025-04-27 23:37:39 +0530

    Seventh Commit

--- src/api/GetApiData.jsx
+++ src/api/GetApiData.jsx
@@ -1,3 +1,19 @@
+// export const getMoviesdata = async()=>{
+//     try{
+//         const response = await fetch(
+//            `https://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&s=titanic&page=1`
+
+//         );
+//         const data =  await response.json();
+//         console.log("Movie data loaded",data);
+//         return data;
+
+//     }catch(error){
+//         console.log(error);
+//         return null;
+//     }
+// }
+
 export const getMoviesdata = async()=>{
     try{
         const response = await fetch(
